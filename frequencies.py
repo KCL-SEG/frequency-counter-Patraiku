ec77aceb3d9b4751ef38ddb6c11ae98d0cd2f137
Patryk Kugla <patryk.kugla@kcl.ac.uk>
2022-10-15 20:01:35 +0100

    Finish task

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if(str(item) not in frequencies.keys()):
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies[str(item)] + 1
     return frequencies
